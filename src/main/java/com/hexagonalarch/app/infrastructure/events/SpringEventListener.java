e7e4eea28c4a4ca3bf9043ded93191fbd8632f48
Marcos Vinicius <marcosab.vinicius@gmail.com>
2022-08-10 00:54:26 -0300

    a way to generate asynchronous events

--- src/main/java/com/hexagonalarch/app/infrastructure/events/SpringEventListener.java
+++ src/main/java/com/hexagonalarch/app/infrastructure/events/SpringEventListener.java
@@ -3,16 +3,18 @@ package com.hexagonalarch.app.infrastructure.events;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
-
 
 @Component
 public class SpringEventListener {
 
     private Logger log = LoggerFactory.getLogger(SpringEventListener.class);
 
+    @Async
     @EventListener
-    public void onApplicationEvent(LogSpringEvent event) {
+    public void onApplicationEvent(LogSpringEvent event) throws InterruptedException {
+        Thread.sleep(5000);
         log.info("Event received " + event.getMessage());
     }
 
